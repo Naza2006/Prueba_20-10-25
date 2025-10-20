59ae024e52ef63e36984f53d03266d1d754e1523
Naza2006 <nazario@uamv.edu.ni>
2025-10-20 07:59:24 -0600

    Interfaz ordenada

--- PilaApp/PilaApp/Form1.Designer.cs
+++ PilaApp/PilaApp/Form1.Designer.cs
@@ -28,77 +28,68 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.label1 = new System.Windows.Forms.Label();
-            this.button1 = new System.Windows.Forms.Button();
-            this.button2 = new System.Windows.Forms.Button();
-            this.button3 = new System.Windows.Forms.Button();
-            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.btnAgregar = new System.Windows.Forms.Button();
+            this.btnEliminar = new System.Windows.Forms.Button();
+            this.btnTope = new System.Windows.Forms.Button();
+            this.Pila = new System.Windows.Forms.ListBox();
             this.tbNombres = new System.Windows.Forms.TextBox();
             this.tbCargos = new System.Windows.Forms.TextBox();
             this.tbSalarios = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
             // 
-            // label1
+            // btnAgregar
             // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(42, 49);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "label1";
+            this.btnAgregar.Location = new System.Drawing.Point(27, 163);
+            this.btnAgregar.Name = "btnAgregar";
+            this.btnAgregar.Size = new System.Drawing.Size(75, 23);
+            this.btnAgregar.TabIndex = 1;
+            this.btnAgregar.Text = "Agregar";
+            this.btnAgregar.UseVisualStyleBackColor = true;
+            this.btnAgregar.Click += new System.EventHandler(this.button1_Click);
             // 
-            // button1
+            // btnEliminar
             // 
-            this.button1.Location = new System.Drawing.Point(45, 95);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(75, 23);
-            this.button1.TabIndex = 1;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.btnEliminar.Location = new System.Drawing.Point(117, 163);
+            this.btnEliminar.Name = "btnEliminar";
+            this.btnEliminar.Size = new System.Drawing.Size(75, 23);
+            this.btnEliminar.TabIndex = 2;
+            this.btnEliminar.Text = "Eliminar";
+            this.btnEliminar.UseVisualStyleBackColor = true;
             // 
-            // button2
+            // btnTope
             // 
-            this.button2.Location = new System.Drawing.Point(126, 104);
-            this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(75, 23);
-            this.button2.TabIndex = 2;
-            this.button2.Text = "button2";
-            this.button2.UseVisualStyleBackColor = true;
+            this.btnTope.Location = new System.Drawing.Point(207, 163);
+            this.btnTope.Name = "btnTope";
+            this.btnTope.Size = new System.Drawing.Size(75, 23);
+            this.btnTope.TabIndex = 3;
+            this.btnTope.Text = "Ver Tope";
+            this.btnTope.UseVisualStyleBackColor = true;
             // 
-            // button3
+            // Pila
             // 
-            this.button3.Location = new System.Drawing.Point(207, 104);
-            this.button3.Name = "button3";
-            this.button3.Size = new System.Drawing.Size(75, 23);
-            this.button3.TabIndex = 3;
-            this.button3.Text = "button3";
-            this.button3.UseVisualStyleBackColor = true;
-            // 
-            // listBox1
-            // 
-            this.listBox1.FormattingEnabled = true;
-            this.listBox1.Location = new System.Drawing.Point(67, 173);
-            this.listBox1.Name = "listBox1";
-            this.listBox1.Size = new System.Drawing.Size(169, 173);
-            this.listBox1.TabIndex = 4;
+            this.Pila.FormattingEnabled = true;
+            this.Pila.Location = new System.Drawing.Point(79, 215);
+            this.Pila.Name = "Pila";
+            this.Pila.Size = new System.Drawing.Size(169, 173);
+            this.Pila.TabIndex = 4;
             // 
             // tbNombres
             // 
-            this.tbNombres.Location = new System.Drawing.Point(126, 12);
+            this.tbNombres.Location = new System.Drawing.Point(126, 61);
             this.tbNombres.Name = "tbNombres";
             this.tbNombres.Size = new System.Drawing.Size(100, 20);
             this.tbNombres.TabIndex = 5;
             // 
             // tbCargos
             // 
-            this.tbCargos.Location = new System.Drawing.Point(126, 38);
+            this.tbCargos.Location = new System.Drawing.Point(126, 87);
             this.tbCargos.Name = "tbCargos";
             this.tbCargos.Size = new System.Drawing.Size(100, 20);
             this.tbCargos.TabIndex = 6;
             // 
             // tbSalarios
             // 
-            this.tbSalarios.Location = new System.Drawing.Point(163, 78);
+            this.tbSalarios.Location = new System.Drawing.Point(126, 113);
             this.tbSalarios.Name = "tbSalarios";
             this.tbSalarios.Size = new System.Drawing.Size(100, 20);
             this.tbSalarios.TabIndex = 7;
@@ -111,11 +102,10 @@
             this.Controls.Add(this.tbSalarios);
             this.Controls.Add(this.tbCargos);
             this.Controls.Add(this.tbNombres);
-            this.Controls.Add(this.listBox1);
-            this.Controls.Add(this.button3);
-            this.Controls.Add(this.button2);
-            this.Controls.Add(this.button1);
-            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Pila);
+            this.Controls.Add(this.btnTope);
+            this.Controls.Add(this.btnEliminar);
+            this.Controls.Add(this.btnAgregar);
             this.Name = "Form1";
             this.Text = "Form1";
             this.ResumeLayout(false);
@@ -124,12 +114,10 @@
         }
 
         #endregion
-
-        private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.Button button2;
-        private System.Windows.Forms.Button button3;
-        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button btnAgregar;
+        private System.Windows.Forms.Button btnEliminar;
+        private System.Windows.Forms.Button btnTope;
+        private System.Windows.Forms.ListBox Pila;
         private System.Windows.Forms.TextBox tbNombres;
         private System.Windows.Forms.TextBox tbCargos;
         private System.Windows.Forms.TextBox tbSalarios;
